2fb8db797a017dc31fdd30f693acf3210c51b37f
JobLinkEducation <144514689+JobLinkEducation@users.noreply.github.com>
2023-09-12 20:08:18 +0530

    Update appointment_booking.py

--- appointment_booking.py
+++ appointment_booking.py
@@ -1,65 +1,33 @@
-hospitals = ["Apollo Hospital", "Fortis Hospital", "Max Hospital"]
-doctors = {
-    "Apollo Hospital": ["Dr. Smith", "Dr. Jones", "Dr. Brown"],
-    "Fortis Hospital": ["Dr. Green", "Dr. Blue", "Dr. Red"],
-    "Max Hospital": ["Dr. White", "Dr. Black", "Dr. Yellow"],
-}
-appointment_times = {
-    "Apollo Hospital": {
-        "Dr. Smith": ["10:00 AM", "11:00 AM", "12:00 PM"],
-        "Dr. Jones": ["2:00 PM", "3:00 PM", "4:00 PM"],
-        "Dr. Brown": ["5:00 PM", "6:00 PM", "7:00 PM"],
-    },
-    "Fortis Hospital": {
-        "Dr. Green": ["10:00 AM", "11:00 AM", "12:00 PM"],
-        "Dr. Blue": ["2:00 PM", "3:00 PM", "4:00 PM"],
-        "Dr. Red": ["5:00 PM", "6:00 PM", "7:00 PM"],
-    },
-    "Max Hospital": {
-        "Dr. White": ["10:00 AM", "11:00 AM", "12:00 PM"],
-        "Dr. Black": ["2:00 PM", "3:00 PM", "4:00 PM"],
-        "Dr. Yellow": ["5:00 PM", "6:00 PM", "7:00 PM"],
-    },
-}
+import random
 
+hospitals = ["Hospital A", "Hospital B", "Hospital C"]
+doctors = ["Doctor 1", "Doctor 2", "Doctor 3"]
+appointment_times = ["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"]
 
-# Get the hospital name
-print("Here are the available hospitals:")
-for hospital in hospitals:
-    print(f"* {hospital}")
+def book_appointment(hospital, doctor, appointment_time):
+  # Check if the appointment time is available.
+  if appointment_time not in appointment_times:
+    return False
 
-hospital_name = input("Enter the hospital name: ")
+  # Book the appointment.
+  appointment_times.remove(appointment_time)
 
-# Check if the hospital name is valid
-if hospital_name not in hospitals:
-    print("Invalid hospital name.")
-    exit()
+  # Return success.
+  return True
 
-# Get the doctor name
-print("Here are the available doctors at {}:".format(hospital_name))
-for doctor in doctors[hospital_name]:
-    print(f"* {doctor}")
+if __name__ == "__main__":
+  # Get the selected hospital and doctor.
+  hospital = input("Enter the selected hospital: ")
+  doctor = input("Enter the selected doctor: ")
 
-doctor_name = input("Enter the doctor name: ")
+  # Get the selected appointment time.
+  appointment_time = input("Enter the selected appointment time: ")
 
-# Check if the doctor name is valid
-if doctor_name not in doctors[hospital_name]:
-    print("Invalid doctor name.")
-    exit()
+  # Book the appointment.
+  result = book_appointment(hospital, doctor, appointment_time)
 
-# Get the appointment time
-print("Here are the available appointment times for {} {}:".format(doctor_name, hospital_name))
-for appointment_time in appointment_times[hospital_name][doctor_name]:
-    print(f"* {appointment_time}")
-
-appointment_time = input("Enter the appointment time: ")
-
-# Check if the appointment time is valid
-if appointment_time not in appointment_times[hospital_name][doctor_name]:
-    print("Invalid appointment time.")
-    exit()
-
-# Update the appointment
-appointment_times[hospital_name][doctor_name].remove(appointment_time)
-
-print("Appointment booked successfully!")
+  # Print the result.
+  if result:
+    print("Appointment booked successfully!")
+  else:
+    print("Failed to book appointment.")
